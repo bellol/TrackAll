fa9a68ca8e54cb7e73aa6ca17fad1c0e6d9ccaaf
Bell Ng <kotomoto@gmail.com>
2015-05-12 02:06:10 +1000

    added add account activity added myki account

---
+++ app/src/main/java/com/example/bellng/trackall/AccountType.java
@@ -0,0 +1,18 @@
+package com.example.bellng.trackall;
+
+/**
+ * Created by bellng on 11/05/2015.
+ */
+public enum AccountType {
+    Myki("Myki");
+
+    private String displayName;
+
+    private AccountType(String displayName){
+        this.displayName = displayName;
+    }
+
+    public String toString(){
+        return displayName;
+    }
+}
